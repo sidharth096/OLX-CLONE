64b6cf07348f0ca7b1e2b13d3a4aeb494d05c3ef
sidharth <sidharthtp096@gmail.com>
2023-07-20 07:51:23 -0800

    design

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import Logo from '../../olx-logo.png';
+import './Signup.css';
+
+export default function Signup() {
+  const [username,setUsername] =useState('')
+  const[email,setEmail]=useState('')
+  const[phone,setPhone]=useState('')
+  const[password,setPassword]=useState('')
+
+  const handleSubmit =()=>{
+    console.log(username);
+  }
+  return (
+    <div>
+      <div className="signupParentDiv">
+        <img width="200px" height="200px" src={Logo}></img>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="fname">Username</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            id="fname"
+            name="name"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            id="fname"
+            name="email"
+            defaultValue="John"
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
+            id="lname"
+            name="phone"
+            defaultValue="Doe"
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            id="lname"
+            name="password"
+            defaultValue="Doe"
+          />
+          <br />
+          <br />
+          <button>Signup</button>
+        </form>
+        <a>Login</a>
+      </div>
+    </div>
+  );
+}
